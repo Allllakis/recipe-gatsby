f3fe5be1b518da15d171f4a8151340fdcf1f0530
Allllakis <allakiselova20@gmail.com>
2021-11-10 22:08:07 +0300

    recipe template

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,14 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import slugify from "slugify"
 const RecipesList = ({ recipes = [] }) => {
   return (
     <div className="recipes-list">
       {recipes.map(recipe => {
           const {image, title, id, prepTime, cookTime} = recipe
           const pathToImage = getImage(image)
+          const slug = slugify(title, {lower: true})
         return (
-          <Link to={`/${title}`} className="recipe" key={id}>
+          <Link to={`/${slug}`} className="recipe" key={id}>
             <GatsbyImage
               image={pathToImage}
               className="recipe-img"
